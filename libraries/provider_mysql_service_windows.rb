dcd0992347f50d57a0e4adcbdf999a0716400c38
Sean OMeara <sean@chef.io>
2015-02-16 21:22:25 -0500

    savegame: successfully generated DSC script, and used latest DSC Resource Kit wave 9

--- libraries/provider_mysql_service_windows.rb
+++ libraries/provider_mysql_service_windows.rb
@@ -15,20 +15,52 @@ class Chef
         end
 
         action :create do
-          log 'hello world'
+
+          template 'c:\check_me_out.txt' do
+            variables(:config => new_resource)
+            source 'check_me_out.erb'
+            cookbook 'mysql'
+            action :create
+          end
 
           remote_file "#{cache_path}\\DSC Resource Kit.zip" do
-            #source 'http://gallery.technet.microsoft.com/scriptcenter/DSC-Resource-Kit-All-c449312d/file/126120/1/DSC%20Resource%20Kit%20Wave%207%2009292014.zip'
             source 'https://gallery.technet.microsoft.com/scriptcenter/DSC-Resource-Kit-All-c449312d/file/131371/1/DSC%20Resource%20Kit%20Wave%209%2012172014.zip'
+            checksum "7eeb0563bc9a82a1fcfb0f991d027192b45788eeaf2263fc1b0836c46c5d1dd8"
           end
-          
+
           dsc_resource 'get-dsc-resource-kit' do
             resource :Archive
             property :ensure, 'Present'
             property :path, "#{cache_path}/DSC Resource Kit.zip"
             property :destination, "#{ENV['PROGRAMW6432']}/WindowsPowerShell/Modules"
           end
-          
+
+          directory "#{cache_path}/mysql" do
+            recursive true
+            action :create
+          end
+
+          template "#{cache_path}/mysql/derp.ps1" do
+            cookbook 'mysql'
+            source 'derp.ps1.erb'
+            variables(
+              :work_dir => cache_path,
+              :mysql_name => mysql_name,
+              :node_name => 'localhost',
+              :ps_dsc_allow_plain_text_password => 'TRUE',
+              :package_product_id => '{437AC169-780B-47A9-86F6-14D43C8F596B}',
+              :package_uri => 'http://dev.mysql.com/get/Downloads/MySQLInstaller/mysql-installer-community-5.6.17.0.msi',
+              :initial_root_password => new_resource.initial_root_password,
+              )
+            action :create
+          end
+
+          powershell_script "Use DSC to install mysql" do
+            cwd "#{cache_path}/mysql"
+            command ". ./derp.ps1"
+            # FIXME: not_if { something }
+          end
+
         end
 
         action :delete do
