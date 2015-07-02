357bce296dfd6d81c222d7fcc80fb8e13c34961f
Jens Rosenboom <j.rosenboom@x-ion.de>
2015-07-02 17:15:05 +0200

    Add support for Ubuntu 15.10 and 16.04
    Should work mostly the same way as 15.04 does.

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -280,6 +280,10 @@ EOSQL
         @pkginfo.set['debian']['14.10']['5.6']['server_package'] = 'mysql-server-5.6'
         @pkginfo.set['debian']['15.04']['5.6']['client_package'] = %w(mysql-client-5.6 libmysqlclient-dev)
         @pkginfo.set['debian']['15.04']['5.6']['server_package'] = 'mysql-server-5.6'
+        @pkginfo.set['debian']['15.10']['5.6']['client_package'] = %w(mysql-client-5.6 libmysqlclient-dev)
+        @pkginfo.set['debian']['15.10']['5.6']['server_package'] = 'mysql-server-5.6'
+        @pkginfo.set['debian']['16.04']['5.6']['client_package'] = %w(mysql-client-5.6 libmysqlclient-dev)
+        @pkginfo.set['debian']['16.04']['5.6']['server_package'] = 'mysql-server-5.6'
         @pkginfo.set['debian']['6']['5.1']['client_package'] = %w(mysql-client libmysqlclient-dev)
         @pkginfo.set['debian']['6']['5.1']['server_package'] = 'mysql-server-5.1'
         @pkginfo.set['debian']['7']['5.5']['client_package'] = %w(mysql-client libmysqlclient-dev)
@@ -454,6 +458,8 @@ EOSQL
       return '5.5' if node['platform_family'] == 'suse'
       return '5.6' if node['platform_family'] == 'fedora'
       return '5.6' if node['platform_family'] == 'debian' && node['platform_version'] == '15.04'
+      return '5.6' if node['platform_family'] == 'debian' && node['platform_version'] == '15.10'
+      return '5.6' if node['platform_family'] == 'debian' && node['platform_version'] == '16.04'
     end
   end
 end
